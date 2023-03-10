79ae50698afacfabf3a823c55af796289366d559
Tanisha <jaiswaltanisha11@gmail.com>
2023-03-10 23:37:02 +0530

    Refactored Project

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 
-const Book = () => {
-  return (
-    <article className='book'>
-    <Image />
-    <Title/>
-    <Author />
-    </article>
-  )
-}
+const author = 'James Clear'
 
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/91bYsX41DVL._AC_UY327_FMwebp_QL65_.jpg"
-    alt="Atomic Habits"
-  />
-)
-const Title = () => <h2>Atomic Habits</h2>
-const Author = () => <h4>James Clear</h4> 
+const Book = () => {
+    const title = 'Atomic Habits'
+
+  return (
+
+    <article className='book'>
+        <img
+            src="https://m.media-amazon.com/images/I/91bYsX41DVL._AC_UY327_FMwebp_QL65_.jpg"
+            alt="Atomic Habits"
+        />
+        <h2>{title}</h2>
+        <h4>{author.toUpperCase()}</h4> 
+    </article>
+  );
+};
+
 export default Book
+
